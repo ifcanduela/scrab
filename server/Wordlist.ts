812e55b667f2cf9131ffa9251d049cc3b009f710
Igor F. Canduela <ifcanduela@gmail.com>
2023-09-25 19:28:59 +0200

    Disable read/write for now

--- server/Wordlist.ts
+++ server/Wordlist.ts
@@ -1,18 +1,20 @@
 import path from "path"
 import fs from "fs/promises"
+import wordlist from "~/public/wordlist.json" assert { type: "json" }
 
 export async function readWords() {
-    const filename = path.resolve(process.cwd(), "public", "wordlist.json")
-    const json = await fs.readFile(filename, "utf-8")
-    const words = JSON.parse(json)
-
-    return words
+	// const filename = path.resolve(process.cwd(), "public", "wordlist.json")
+	// const json = await fs.readFile(filename, "utf-8")
+	// const words = JSON.parse(json)
+	//
+	// return words
+	return wordlist
 }
 
 export async function writeWords(words: string[]) {
-    const filename = path.resolve(process.cwd(), "public", "wordlist.json")
-    const json = JSON.stringify(words, null, 4)
-    const result = await fs.writeFile(filename, json)
+	const filename = path.resolve(process.cwd(), "public", "wordlist.json")
+	const json = JSON.stringify(words, null, 4)
+	const result = await fs.writeFile(filename, json)
 
-    return result
+	return result
 }
