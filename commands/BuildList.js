18a9b9e716c5186f24ab4314a8b81288cacbdc46
Igor F. Canduela <ifcanduela@gmail.com>
2023-11-04 16:28:28 +0100

    Match using wildcards

--- commands/BuildList.js
+++ commands/BuildList.js
@@ -1,30 +1,42 @@
 import fs from "fs/promises"
 
-async function buildListCommand(_params, program) {
-	const textFile = await fs.readFile("./data/opentaal/wordlist.txt", "utf-8")
-	const allWords = textFile
+function cleanFileContent(textFile) {
+	return textFile
 		.toString()
 		.split("\n")
-		.map((w) => w.trim())
+		.map((w) => w.trim().toLowerCase())
 		.filter((w) => !w.match(/\d/))
 		.filter((w) => !w.match(/\s/))
 		.filter((w) => w.length > 1)
+}
+
+async function buildListCommand(_params, program) {
+	const basicFile = await fs.readFile(
+		"./data/opentaal/basiswoorden-gekeurd.txt",
+		"utf-8",
+	)
+	const basicList = cleanFileContent(basicFile)
+
+	const derivativeFile = await fs.readFile(
+		"./data/opentaal/flexies-ongekeurd.txt",
+		"utf-8",
+	)
+	const derivativeList = cleanFileContent(derivativeFile)
 
 	const allPlacesJson = await fetch(
 		"https://opendata.cbs.nl/ODataApi/OData/84734NED/Woonplaatsen",
 	)
 	const allPlacesData = await allPlacesJson.json()
-
-	allPlacesData.value
-		.map((item) => item.Title)
-		.map((w) => w.trim())
+	const placeList = allPlacesData.value
+		.map((item) => item.Title.trim().toLowerCase())
 		.filter((w) => !w.match(/\d/))
 		.filter((w) => !w.match(/\s/))
-		.forEach((w) => {
-			allWords.push(w)
-		})
 
-	fs.writeFile("./data/wordlist.json", JSON.stringify(allWords, null, 2))
+	const allWords = [...basicList, ...derivativeList, ...placeList]
+
+	const unique = Array.from(new Set(allWords))
+
+	fs.writeFile("./data/wordlist.json", JSON.stringify(unique, null, 2))
 }
 
 export default buildListCommand
--- commands/FindWords.js
+++ commands/FindWords.js
@@ -2,7 +2,7 @@ import fs from "fs/promises"
 import WordMatcher from "../lib/WordMatcher.js"
 
 async function findWordsCommand(_params, program) {
-	const json = await fs.readFile("public/wordlist.json", "utf-8")
+	const json = await fs.readFile("data/wordlist.json", "utf-8")
 	const allWords = JSON.parse(json)
 
 	const opts = program.opts()
@@ -17,7 +17,7 @@ async function findWordsCommand(_params, program) {
 		wordMatcher.matchEnd(opts.end)
 	}
 
-	wordMatcher.useWildcard(!!opts.wildcard)
+	wordMatcher.useWildcard(0)
 
 	const result = wordMatcher.match(opts.letters)
 
